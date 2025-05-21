65434d590db3a0d5ce69d2a1795f5137fd2f2767
Pranesh Chandrasekaran <125540554+PraneshC2005@users.noreply.github.com>
2025-05-21 14:27:47 +0530

    renames

--- ToDo-Backend/server.js
+++ ToDo-Backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/todolist')
 .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB!');
 })
 .catch((err) => {
     console.log('Failed to connect to MongoDB', err);
